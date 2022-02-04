334e1cb560900193ed629886e0554b1b56256a72
Yuri Mayk <yuri.mayk@gmail.com>
2022-02-04 15:27:26 -0300

    Created a function to change the name of currency and the recpective flag when the select be changed. Added a EventListner to this select. Created euro and bitcoin consts.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,49 @@
 const button = document.getElementById("converter-button")
+const select = document.getElementById("currency-select")
 const dolar = 5.2
+const euro = 6.7
+const bitcoin = 600.1
 
 const convertValues = () => {
-const input = document.getElementById("input-real").value
-const realValueText = document.getElementById('real-value-text')
-const currencyValueText = document.getElementById('currency-value-text')
-realValueText.innerHTML = input
-currencyValueText.innerHTML = input/dolar
+    const input = document.getElementById("input-real").value
+    const realValueText = document.getElementById('real-value-text')
+    const currencyValueText = document.getElementById('currency-value-text')
+    
+    realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
+        {
+            style: 'currency',
+            currency: 'BRL'
+        }
+    ).format(input)
+
+    currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
+        {
+            style: 'currency',
+            currency: 'USD'
+        }
+    ).format(input / dolar)
+
+}
+
+const changeCurrency = () => {
+    const currencyName = document.getElementById("currency-name")
+const currencyImage = document.getElementById("currency-image")
+
+    if(select.value === "US$ Dólar Americano") {
+        currencyName.innerHTML = "Dólar Americano"
+        currencyImage.src = "./assets/Eua.png"
+    }
+
+    if(select.value === "€ Euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "./assets/Euro.png"
+    }
+
+    if(select.value === "₿ Bitcoin") {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImage.src = "./assets/bitcoin.png"
+    }
 }
 
 button.addEventListener('click', convertValues)
+select.addEventListener('change',changeCurrency)
